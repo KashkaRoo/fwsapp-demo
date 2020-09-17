3df4f2b0998f785469fb0672e8587825f2b9cce1
Heather Rosa <rosah@sou.edu>
2020-09-16 21:14:36 -0700

    edit gitignore

---
+++ fws-app/build/precache-manifest.11c2ce8a7561e75b8b72606a066ece47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3caf721f4618b5a7edd69c096295c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "655016af6d58c241f814",
+    "url": "/static/css/2.8c1f6bf6.chunk.css"
+  },
+  {
+    "revision": "0d833003e415141e809e",
+    "url": "/static/css/main.37ae2391.chunk.css"
+  },
+  {
+    "revision": "655016af6d58c241f814",
+    "url": "/static/js/2.4a2a59f8.chunk.js"
+  },
+  {
+    "revision": "c405dcce58b05fccaf59ca9b522dd1e7",
+    "url": "/static/js/2.4a2a59f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d833003e415141e809e",
+    "url": "/static/js/main.24e714c7.chunk.js"
+  },
+  {
+    "revision": "afad8627c76efca0cadd",
+    "url": "/static/js/runtime-main.fdd81af0.js"
+  }
+]);
